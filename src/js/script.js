b5c25eca8b15d9fc6124344cc93a0e7a7266230a
Adachima <sapondanaisriwan@gmail.com>
2023-04-13 01:49:15 +0700

    .

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+// const link = document.querySelector('[href="#converter-container"]');
+// console.log(link)
+
+// link[0].addEvenListener("click", (event) => {
+//   event.preventDefault();
+// });
